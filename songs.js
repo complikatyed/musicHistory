59710905036af1a4cb5900005167414e9f26a128
complikatyed <katye.russell@gmail.com>
2016-02-18 15:44:49 -0600

    Add functions to display songs

--- songs.js
+++ songs.js
@@ -1,7 +1,33 @@
 var songs = [];
 
-songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+songs[0] = ["Legs", "ZZTop", "Eliminator", "rock"];
+songs[1] = ["The Logical Song", "Supertramp", "Breakfast in America", "rock"];
+songs[2] = ["Another Brick in the Wall", "Pink Floyd", "The Wall", "rock"];
+songs[3] = ["Welcome to the Jungle","Guns & Roses", "Appetite for Destruction", "rock"];
+songs[4] = ["Ironic", "Alanis Morisette", "Jagged Little Pill", "rock"];
+
+
+
+var buildSongString = function(myArray) {
+  mySongs = "";
+  for (var i = 0; i < myArray.length; i++) {
+
+   mySongs += "<section><p class='title'>" + myArray[i][0] + "</p>";
+   mySongs += "<ul class='song'>";
+   mySongs += "<li>" + myArray[i][1] + "</li>";   // Artist name
+   mySongs += "<li>" + myArray[i][2] + "</li>";   // Album name
+   mySongs += "<li>" + myArray[i][3] + "</li>";   // Song genre
+   mySongs += "</ul></section>";
+  }
+   return mySongs;
+
+}
+
+
+var displaySongs = function(myArray, myId) {
+  songSpot = document.getElementById(myId);
+
+  songSpot.innerHTML = buildSongString(myArray);
+}
+
+displaySongs(songs, "songchart");
