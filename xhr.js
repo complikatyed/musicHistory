a9eefea8973851393c43b08ef73b71930664aa9e
complikatyed <katye.russell@gmail.com>
2016-03-30 17:56:06 -0500

    List songs now functioning with add methods

--- xhr.js
+++ xhr.js
@@ -1,25 +1,33 @@
-function executeThisCodeAfterFileIsLoaded () {
-  var myText = JSON.parse(this.responseText);
+'use strict'
 
-  showSongs(myText.songs, "songchart");
+
+function onLoadSuccess () {
+
+  var myText = JSON.parse(this.responseText);
+  gatherStoredSongData(myText.songs);
 }
 
-function executeThisCodeIfXHRFails () {
+function onLoadFailure () {
+
   alert("Well, that didn't work.");
+
 }
 
 // Create an XHR object
 var myRequest = new XMLHttpRequest();
 
 // "load" means the data was successfully loaded, so the first function runs
-myRequest.addEventListener("load", executeThisCodeAfterFileIsLoaded);
+myRequest.addEventListener("load", onLoadSuccess);
 
 // "error" means the data did NOT load, so the second function (w alert) runs
-myRequest.addEventListener("error", executeThisCodeIfXHRFails);
+myRequest.addEventListener("error", onLoadFailure);
 
 // Then tell the XHR object exactly what to do
 myRequest.open("GET", "songs.json");
 
 // Tell the XHR object to start
 myRequest.send();
+
+
+
 
