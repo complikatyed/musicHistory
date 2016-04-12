49792c15f92b43f5ee39b9f4b7916d04d312b606
complikatyed <katye.russell@gmail.com>
2016-04-11 20:32:44 -0500

    Refactored to use jquery

--- addSong.js
+++ addSong.js
@@ -1,70 +1,71 @@
 'use strict'
 
-let addLink = document.getElementById("link-addMusic");
-let addView = document.getElementById("view-addMusic");
+$(document).ready(function(){
 
-let songsArray = [];
+  let songsArray = [];
 
-// -------------------- Event listeners for the 'List Music' and 'Add Music' buttons -------- //
+  // -------------------- Event listeners for the 'List Music' and 'Add Music' buttons -------- //
 
-addLink.addEventListener("click", function() {
-  listView.classList.add("hidden");
+  $("#link-addMusic").click(function() {
+    $("#view-listMusic").addClass("hidden");
 
-  addView.classList.add("visible");
-  addView.classList.remove("hidden");
+    $('#view-addMusic').addClass("visible");
+    $('#view-addMusic').removeClass("hidden");
 
+  });
+
+
+
+
+  // -------------------- Capture values from user "add songs" input  ---------------------- //
+
+  var getSongs = function() {
+    let title = $("#songTitle").value;
+    let artist = $("#songArtist").value;
+    let album = $("#songAlbum").value;
+    let genre = $("#songGenre").value.toLowerCase();
+
+    var inputArray = [];
+
+    // ----- create a new song array (that will go in the songs array)
+
+    inputArray.push(title, artist, album, genre);
+
+    // ----- add new song array to the master songs array
+
+    songsArray.push(inputArray);
+
+    // ----- this function is defined in list.js 
+    buildSongCardsFromObject(songsArray);
+  }
+
+  // -------------------- Clear 'Add Songs' input boxes ----------------------------- //
+
+  var clearInputs = function() {
+
+    $("#songTitle").value = "";
+    $("#songArtist").value = "";
+    $("#songAlbum").value = "";
+    $("#songGenre").value = "";
+
+  }
+
+  // -------------------- Event listener for the "Add" button -------------------------- //
+
+
+  $("#addBtn").click(function() {
+
+    getSongs();
+    clearInputs();
+
+    $('#view-addMusic').addClass("hidden");
+    $('#view-addMusic').removeClass("visible");
+
+    $("#view-listMusic").addClass("visible");
+    $("#view-listMusic").removeClass("hidden");
+
+  });
 });
-
-// -------------------- Capture values from user "add songs" input  ---------------------- //
-
-var getSongs = function() {
-  let title = document.getElementById("songTitle").value;
-  let artist = document.getElementById("songArtist").value;
-  let album = document.getElementById("songAlbum").value;
-  let genre = document.getElementById("songGenre").value.toLowerCase();
-
-  var inputArray = [];
-
-  // ----- create a new song array (that will go in the songs array)
-
-  inputArray.push(title, artist, album, genre);
-
-  // ----- add new song array to the master songs array
-
-  songsArray.push(inputArray);
-
-  // ----- this function is defined in list.js 
-  buildSongCardsFromObject(songsArray);
-}
-
-// -------------------- Clear 'Add Songs' input boxes ----------------------------- //
-
-var clearInputs = function() {
-
-  document.getElementById("songTitle").value = "";
-  document.getElementById("songArtist").value = "";
-  document.getElementById("songAlbum").value = "";
-  document.getElementById("songGenre").value = "";
-
-}
-
-// -------------------- Event listener for the "Add" button -------------------------- //
-
-var addBtn = document.getElementById("addBtn");
-
-addBtn.addEventListener("click", function() {
-
-  getSongs();
-  clearInputs();
-
-  addView.classList.add("hidden");
-  addView.classList.remove("visible");
-
-  listView.classList.add("visible");
-  listView.classList.remove("hidden");
-
-});
-
 
 
 
